011984bd28fd7b86a509611ded5b733ed196148a
dmolchanenko <dmitrim@primatest.net>
2018-06-26 09:15:03 -0700

    added more smart contract test cases

--- src/test/groovy/api/smartcontracts/RegressionSmartContracts.groovy
+++ src/test/groovy/api/smartcontracts/RegressionSmartContracts.groovy
@@ -1,9 +1,12 @@
 package api.smartcontracts
 
 import com.dispatchlabs.io.testing.common.NodeSetup
+import com.dispatchlabs.io.testing.common.contracts.ComplexContract
 import com.dispatchlabs.io.testing.common.contracts.DefaultSampleContract
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
+
+import javax.xml.bind.DatatypeConverter
 
 import static com.dispatchlabs.io.testing.common.APIs.*
 import static org.hamcrest.Matchers.equalTo
@@ -74,7 +77,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Pass invalid value for parameter type: hash",groups = ["smart contract"])
-    public void transactionRegression8_SmartContract(){
+    public void SmartContract_API22(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -86,7 +89,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Pass invalid value for parameter type: integer",groups = ["smart contract"])
-    public void transactionRegression9_SmartContract(){
+    public void SmartContract_API20(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -98,7 +101,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Pass invalid value for parameter type: boolean",groups = ["smart contract"])
-    public void transactionRegression10_SmartContract(){
+    public void SmartContract_API24(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -110,7 +113,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Contract returns no value",groups = ["smart contract"])
-    public void transactionRegression11_SmartContract(){
+    public void SmartContract_API26(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -123,7 +126,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Negative: Pass only some parameter values for methods",groups = ["smart contract"])
-    public void transactionRegression12_SmartContract(){
+    public void SmartContract_API28(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -136,7 +139,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Deploy exact same contract",groups = ["smart contract"])
-    public void transactionRegression13_SmartContract(){
+    public void SmartContract_API29(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -161,7 +164,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Negative: Empty ABI on contract method call",groups = ["smart contract"])
-    public void transactionRegression14_SmartContract(){
+    public void SmartContract_API31(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -173,7 +176,7 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Negative: Invalid values for ABI",groups = ["smart contract"])
-    public void transactionRegression15_SmartContract(){
+    public void SmartContract_API32(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample
         response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
@@ -186,16 +189,58 @@ class RegressionSmartContracts {
     }
 
     @Test(description="Negative: Empty smart contract",groups = ["smart contract"])
-    public void transactionRegression16_SmartContract(){
+    public void SmartContract_API35(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:""
         waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
     }
 
     @Test(description="Negative: Partial bytecode of valid contract",groups = ["smart contract"])
-    public void transactionRegression17_SmartContract(){
+    public void SmartContract_API36(){
         def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
                 Code:DefaultSampleContract.defaultSample.substring(0,DefaultSampleContract.defaultSample.size()-10)
         response.then().assertThat().body("type",equalTo("InvalidTransaction"))
+    }
+
+    @Test(description="Contract returns multiple values (2 strings)",groups = ["smart contract"])
+    public void SmartContract_API106(){
+        def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
+                Code:ComplexContract.contract
+        response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        def contractAddress1 = response.then().extract().path("contractAddress")
+        response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:contractAddress1, PrivateKey:"Genesis",Type:2,
+                ABI: ComplexContract.abi,
+                Method: "getMultiReturn",Params: []
+        def getID = response.then().extract().path("id")
+        waitForTransactionStatus ID:getID ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        verifyStatusForTransaction(Nodes:[allNodes.Delegates.Delegate0],ID:getID,ContractResult:["test1","test2"])
+    }
+
+    @Test(description="Contract method does logging (emit the event)",groups = ["smart contract"])
+    public void SmartContract_API108(){
+        def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
+                Code:ComplexContract.contract
+        response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        def contractAddress1 = response.then().extract().path("contractAddress")
+        response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:contractAddress1, PrivateKey:"Genesis",Type:2,
+                ABI: ComplexContract.abi,
+                Method: "logEvent",Params: []
+        def getID = response.then().extract().path("id")
+        waitForTransactionStatus ID:getID ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        verifyStatusForTransaction(Nodes:[allNodes.Delegates.Delegate0],ID:getID,ContractResult:"")
+    }
+
+    @Test(description="Negative: Call out method which does not exist",groups = ["smart contract"])
+    public void SmartContract_API107(){
+        def response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:"", PrivateKey:"Genesis",Type:1,
+                Code:ComplexContract.contract
+        response = waitForTransactionStatus ID:response.then().extract().path("id") ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        def contractAddress1 = response.then().extract().path("contractAddress")
+        response = sendTransaction Node:allNodes.Delegates.Delegate0, Value:0,From:"Genesis",To:contractAddress1, PrivateKey:"Genesis",Type:2,
+                ABI: ComplexContract.abi,
+                Method: "notThere",Params: []
+        def getID = response.then().extract().path("id")
+        waitForTransactionStatus ID:getID ,Node:allNodes.Delegates.Delegate0, Status: "Ok", Timeout: 10
+        verifyStatusForTransaction(Nodes:[allNodes.Delegates.Delegate0],ID:getID,ContractResult:"")
     }
 }
